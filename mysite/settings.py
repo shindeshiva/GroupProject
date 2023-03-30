6e4d8d96665b6a6add805c03d0e101fdc33cf65b
CHARLES BOTA ANKOMAH <c.ankomah.22@abdn.ac.uk>
2023-03-30 14:31:38 +0000

    adding allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-au!chycg)p@#u*+my3%s^qql87cro)_a&+_qv625g^hug7iuv2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'americaqueen-distantmarket-8000.codio-box.uk', 'changerocket-clarionemotion-8000.codio-box.uk']
+ALLOWED_HOSTS = ['americaqueen-distantmarket-8000.codio-box.uk', 'changerocket-clarionemotion-8000.codio-box.uk', 'financearchive-karmakarl-80.codio-box.uk']
 
 
 # Application definition
