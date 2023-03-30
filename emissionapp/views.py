aeb84571e54a67f99273563244fe4841116ccf6b
QIXUAN WU <q.wu.22@abdn.ac.uk>
2023-03-30 12:11:05 +0000

    connect database and templates

--- emissionapp/views.py
+++ emissionapp/views.py
@@ -4,3 +4,13 @@ from .models import countrydata
 def display_country_data(request):
     users = countrydata.objects.all()
     return render(request, 'emissionapp/countrydata.html', {'users' : users})
+
+
+
+def display_index(request):
+    return render(request, 'emissionapp/index.html')
+
+def display_value_data(request):
+    # values = value.objects.all()
+    return render(request, 'emissionapp/value.html')
+
